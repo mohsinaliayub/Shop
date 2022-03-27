94f44fb04aab5b25134fa2f9e0dcbe986e3c4b62
Mohsin Ali Ayub <meismosin@gmail.com>
2022-03-27 15:25:22 +0200

    add item controller, and create outlets and actions

---
+++ Shop/Controller/AddItemViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddItemViewController.swift
+//  Shop
+//
+//  Created by Mohsin Ali Ayub on 27.03.22.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+    
+    // outlets
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func saveItem(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func cameraButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        
+    }
+    
+}
